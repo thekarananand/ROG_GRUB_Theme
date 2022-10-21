cfba6fadefac8f1f526665e91f7e6b442b07ac5b
Karan Anand <thekarananand@gmail.com>
2022-10-21 08:50:42 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ sed -i '/GRUB_TIMEOUT_STYLE=/d' /etc/default/grub
 echo "GRUB_THEME=\"/boot/grub/themes/ROG/theme.txt\"" >> /etc/default/grub
 echo 'GRUB_GFXMODE="auto"' >> /etc/default/grub 
 echo 'GRUB_TIMEOUT="10"' >> /etc/default/grub
-sed -i '/GRUB_TIMEOUT=/d' /etc/default/grub
+echo 'GRUB_TIMEOUT_STYLE="menu"' >> /etc/default/grub
 
 update-grub
